ece577db4861c0c821fd255cd2438950b119880d
GhanemYassine <46890231+GhanemYassine@users.noreply.github.com>
2020-11-25 12:58:12 +0100

    styling

--- src/Content.jsx
+++ src/Content.jsx
@@ -10,6 +10,10 @@ import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import { makeStyles } from '@material-ui/core/styles';
 import CardContent from '@material-ui/core/CardContent';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 
 const useStyles = makeStyles({
     root: {
@@ -27,13 +31,22 @@ const Content = (props) => {
 
 
         <div className="content">
-            <h4>Totale duur: {CHANNELS[props.whosIsActive].slides.reduce((acc, curr) => acc + curr.duration, 0)} m</h4>
+            <h4>Totale duur: {CHANNELS[props.whosIsActive].slides.reduce((acc, curr) => acc + curr.duration, 0)} min</h4>
             <div className="rightside">
-                <p style={{ marginLeft: '0.5rem' }}>  <PlayCircleOutlineIcon /> <span style={{ marginTop: '-2.5rem' }}>Preview the entire channel</span></p>
+                <p style={{ marginLeft: '0.5rem' }}>  <PlayCircleOutlineIcon /> <span style={{ marginTop: '-2.5rem' }}>Preview Channel</span></p>
+                <p style={{ marginLeft: '0.5rem' }}>  <AddCircleOutlineIcon /> <span style={{ marginTop: '-2.5rem' }}> Add Channel</span></p>
+
             </div>
             <Divider />
             <h1 style={{ color: '#FF007F' }}> - {props.whosIsActive}</h1>
-            <p> {CHANNELS[props.whosIsActive].slides.reduce((acc, curr) => acc + curr.duration, 0)} m</p>
+            <p> {CHANNELS[props.whosIsActive].slides.reduce((acc, curr) => acc + curr.duration, 0)} min</p>
+            <div className="rightside">
+                <EditIcon />
+                <DeleteIcon />
+                <VisibilityIcon />
+                <p style={{ marginLeft: '0.5rem' }}>  <CloudUploadIcon /> <span style={{ marginTop: '-2.5rem' }}> Publish</span></p>
+            </div>
+
             <Divider />
             <br></br>
             <div>
@@ -67,6 +80,40 @@ const Content = (props) => {
 
             </div>
             <Divider />
+            <div className="Content">
+                <h1 style={{ color: '#FF007F' }}> - {props.whosIsActive}</h1>
+                <p> {CHANNELS[props.whosIsActive].slides.reduce((acc, curr) => acc + curr.duration, 0)} min</p>
+                <div className="rightsidetwo">
+                    <EditIcon />
+                    <DeleteIcon />
+                    <VisibilityIcon />
+                    <p style={{ marginLeft: '0.5rem' }}>  <CloudUploadIcon /> <span style={{ marginTop: '0' }}> Publish</span></p>
+                </div>
+            </div>
+            <Divider />
+            <Grid container spacing={3}>
+                {CHANNELS[props.whosIsActive].slides.map(slide => {
+                    return (
+                        <Grid item xs={3}>
+                            <Slide name={slide.name} img={slide.img} duration={slide.duration} />
+                        </Grid>
+                    )
+                })}
+                <Grid item xs={3}>
+                    <Card>
+                        <CardMedia
+                            className={classes.media}
+                            image='https://www.icone-png.com/png/30/29947.png'
+                        />
+                        <CardContent>
+                            <p>  Add another picture</p>
+                        </CardContent>
+
+                    </Card>
+                </Grid>
+
+
+            </Grid>
 
         </div >
 
